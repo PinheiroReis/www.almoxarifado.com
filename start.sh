277a56b035e2dc33d97b13ad21c057e79a09f70f
João Victor <joaovictorpinheiro2007@gmail.com>
2025-11-12 20:17:04 -0300

    Initial commit

---
+++ start.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
+
+cd $SCRIPT_DIR
+
+echo "--- Executing API entrypoint ---"
+
+# echo "--- Collecting static files ---"
+# uv run python manage.py collectstatic --noinput
+
+echo "--- Making migrations ---"
+uv run python manage.py makemigrations
+
+echo "--- Migrating ---"
+uv run python manage.py migrate
+
+# echo "--- Running API with gunicorn ---"
+# uv run gunicorn core.wsgi:application --bind 0.0.0.0:8000
+
+echo "--- Running API with gunicorn ---"
+uv run python manage.py runserver
